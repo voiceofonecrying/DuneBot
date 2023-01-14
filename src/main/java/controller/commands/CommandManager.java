4688805cc2eb61ecccdc917a0818d0891af953b0
Leo Greyz <leogreyz@users.noreply.github.com>
2023-01-13 19:07:39 -0600

    Removed red outline from rotated objects.

--- src/main/java/controller/commands/CommandManager.java
+++ src/main/java/controller/commands/CommandManager.java
@@ -1510,8 +1510,6 @@ public class CommandManager extends ListenerAdapter {
         at.rotate(rads, x, y);
         g2d.setTransform(at);
         g2d.drawImage(img, 0, 0, null);
-        g2d.setColor(Color.RED);
-        g2d.drawRect(0, 0, newWidth - 1, newHeight - 1);
         g2d.dispose();
 
         return rotated;
