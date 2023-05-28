7a4dd508b2a7fbaf059fa04ddfcb4ebfb1755c9f
Leo Greyz <leogreyz@users.noreply.github.com>
2023-05-28 13:56:10 -0500

    Adding a file that didn't get added in the last PR.

---
+++ src/main/java/enums/GameOption.java
@@ -0,0 +1,11 @@
+package enums;
+
+public enum GameOption {
+    LEADER_SKILLS,
+    STRONGHOLD_SKILLS,
+    TECH_TOKENS,
+    SANDTROUT,
+    EXPANSION_TREACHERY_CARDS,
+    CHEAP_HERO_TRAITOR,
+    HARKONNEN_MULLIGAN,
+}
