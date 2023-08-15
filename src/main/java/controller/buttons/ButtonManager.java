42d704b8a6e35d5893ed657ce039aaa66662fec2
James Kinder <jmkgfk@gmail.com>
2023-08-14 23:04:35 -0400

    Fix: getCrossShipFrom returns null and throws NullPointerException

--- src/main/java/controller/buttons/ButtonManager.java
+++ src/main/java/controller/buttons/ButtonManager.java
@@ -1,23 +1,15 @@
 package controller.buttons;
 
-import constants.Emojis;
 import exceptions.ChannelNotFoundException;
 import exceptions.InvalidOptionException;
-import model.*;
+import model.DiscordGame;
+import model.Game;
 import model.factions.Faction;
-import model.factions.RicheseFaction;
-import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import net.dv8tion.jda.api.interactions.components.buttons.Button;
-import net.dv8tion.jda.api.interactions.components.buttons.ButtonInteraction;
-import net.dv8tion.jda.api.requests.restaction.MessageCreateAction;
-import net.dv8tion.jda.api.requests.restaction.WebhookMessageCreateAction;
-import net.dv8tion.jda.api.utils.messages.MessageCreateRequest;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
-import java.util.*;
 
 public class ButtonManager extends ListenerAdapter {
     @Override
