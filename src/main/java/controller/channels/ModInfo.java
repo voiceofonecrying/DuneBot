e895c599646ad8b2370bfa64b2a3e87ce83bbaa5
Tom Hayosh <thayosh@gmail.com>
2023-09-28 19:10:34 -0500

    Channel class for mod-info

---
+++ src/main/java/controller/channels/ModInfo.java
@@ -0,0 +1,11 @@
+package controller.channels;
+
+import exceptions.ChannelNotFoundException;
+import model.DiscordGame;
+
+public class ModInfo extends DiscordChannel {
+    public ModInfo(DiscordGame discordGame) throws ChannelNotFoundException {
+        super(discordGame);
+        this.messageChannel = discordGame.getTextChannel("mod-info");
+    }
+}
