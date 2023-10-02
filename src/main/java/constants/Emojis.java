fd15dd54c402cb3b52e4addf440da668f625d9d6
James Kinder <jmkgfk@gmail.com>
2023-10-02 13:01:04 -0400

    Ecaz, Moritani troops and No Field emojis are available.

--- src/main/java/constants/Emojis.java
+++ src/main/java/constants/Emojis.java
@@ -11,7 +11,7 @@ public final class Emojis {
     public static final String CHOAM = "<:choam:991763324624703538>";
     public static final String CHOAM_TROOP = "<:choam_troop:998567370350141492>";
     public static final String ECAZ = "<:ecaz:1142126129105346590>";
-    public static final String ECAZ_TROOP = "Ecaz Troop";
+    public static final String ECAZ_TROOP = "<:ecaz_troop:1153726788011298928>";
     public static final String EMPEROR = "<:emperor:991763323454500914>";
     public static final String EMPEROR_TROOP = "<:emperor_troop:998567367565115504>";
     public static final String EMPEROR_SARDAUKAR = "<:emperor_sardaukar:998567368739541113>";
@@ -26,7 +26,7 @@ public final class Emojis {
     public static final String IX_SUBOID = "<:ixian_suboid:998567360858427512>";
     public static final String IX_CYBORG = "<:ixian_cyborg:998567362024456254>";
     public static final String MORITANI = "<:moritani:1142126199775182879>";
-    public static final String MORITANI_TROOP = "Moritani Troop";
+    public static final String MORITANI_TROOP = "<:moritani_troop:1153670660552413235>";
     public static final String RICHESE = "<:rich:991763318467465337>";
     public static final String RICHESE_TROOP = "<:richese_troop:998567359579160606>";
     public static final String SPICE = "<:spice4:991763531798167573>";
@@ -38,7 +38,7 @@ public final class Emojis {
     public static final String HEIGHLINERS = "<:heighliners:991763495567769640>";
     public static final String SPICE_PRODUCTION = "<:spiceproduction:991763494267531304>";
     public static final String TRANSPARENT_WORM = "<:transparent_worm:991762942536187964>";
-    public static final String NO_FIELD = "No Field Token";
+    public static final String NO_FIELD = "<:no_field:1121583397937106995>";
 
     private Emojis() {
     }
