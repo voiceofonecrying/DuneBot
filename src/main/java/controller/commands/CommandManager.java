f53ea29decf5ce91f406ca82db44a9b2d09f6b3e
Leo Greyz <leogreyz@users.noreply.github.com>
2023-11-02 22:25:08 -0500

    Stop asking bg about 2 advisors to sink (#570)
    * Stop asking BG if they want to ship 2 advisors to the Sink if Homeworlds are disabled.
    
    * Don't have to check for threshold.

--- src/main/java/controller/commands/CommandManager.java
+++ src/main/java/controller/commands/CommandManager.java
@@ -328,14 +328,22 @@ public class CommandManager extends ListenerAdapter {
             }
 
             TurnSummary turnSummary = discordGame.getTurnSummary();
-            if (game.hasFaction("BG") && !(targetFaction.getName().equals("BG") || targetFaction.getName().equals("Fremen"))
-                    && !(game.hasGameOption(GameOption.HOMEWORLDS) && !game.getFaction("BG").isHighThreshold()
-                    && !game.getHomeworlds().containsValue(targetTerritory.getTerritoryName()))) {
+            if (game.hasFaction("BG")
+                    && !(targetFaction.getName().equals("BG") || targetFaction.getName().equals("Fremen"))
+                    && !(
+                            game.hasGameOption(GameOption.HOMEWORLDS)
+                            && !game.getFaction("BG").isHighThreshold()
+                            && !game.getHomeworlds().containsValue(targetTerritory.getTerritoryName())
+                    )) {
                 List<Button> buttons = new LinkedList<>();
                 String territoryName = targetTerritory.getTerritoryName();
                 buttons.add(Button.primary("bg-advise-" + territoryName, "Advise"));
                 buttons.add(Button.secondary("bg-advise-Polar Sink", "Advise to Polar Sink"));
-                buttons.add(Button.secondary("bg-ht", "Advise 2 to Polar Sink"));
+
+                if (game.hasGameOption(GameOption.HOMEWORLDS)) {
+                    buttons.add(Button.secondary("bg-ht", "Advise 2 to Polar Sink"));
+                }
+
                 buttons.add(Button.danger("bg-dont-advise-" + territoryName, "No"));
                 discordGame.getBGChat().queueMessage(Emojis.BG + " Would you like to advise the shipment to " + territoryName + "?" + game.getFaction("BG").getPlayer(), buttons);
             }
