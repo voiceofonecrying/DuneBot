fe9b8d6bd2a01507c24055465852e4d5c16ded14
Tom Hayosh <thayosh@gmail.com>
2023-11-15 16:59:32 -0600

    New function gets lists of sectors to check for battles

--- src/main/java/model/Territories.java
+++ src/main/java/model/Territories.java
@@ -1,12 +1,52 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Territories extends HashMap<String, Territory> {
 
+    /**
+     * Returns the territory with the given name.
+     *
+     * @param name The territory's name.
+     * @return The territory with the given name.
+     */
+    public Territory getTerritory(String name) {
+        Territory territory = get(name);
+        if (territory == null) {
+            throw new IllegalArgumentException("No territory with name " + name);
+        }
+        return territory;
+    }
+
     public Set<String> getDistinctAggregateTerritoryNames() {
         return values().stream().map(Territory::getAggregateTerritoryName).collect(Collectors.toSet());
+    }
+
+    public List<Territory> getTerritorySectors(String aggregateTerritoryName){
+        return values().stream().filter(t -> t.getTerritoryName().indexOf(aggregateTerritoryName) == 0).toList();
+    }
+
+    public List<List<Territory>> getTerritorySectorsForBattle(String aggregateTerritoryName, int storm) {
+        List<Territory> territorySectors = getTerritorySectors(aggregateTerritoryName);
+        List<Territory> sectorsBeforeStorm;
+        List<Territory> sectorsAfterStorm;
+        if (storm != 1 && (aggregateTerritoryName.equals("Cielago North") || aggregateTerritoryName.equals("Cielago Depression"))) {
+            sectorsBeforeStorm = territorySectors.stream().filter(t -> t.getSector() != storm).toList();
+            sectorsAfterStorm = new ArrayList<>();
+        } else if (storm == 1 && (aggregateTerritoryName.equals("Cielago North") || aggregateTerritoryName.equals("Cielago Depression"))) {
+            sectorsBeforeStorm = territorySectors.stream().filter(t -> t.getSector() == 18).toList();
+            sectorsAfterStorm = territorySectors.stream().filter(t -> t.getSector() == 2).toList();
+        } else {
+            sectorsBeforeStorm = territorySectors.stream().filter(t -> t.getSector() < storm).toList();
+            sectorsAfterStorm = territorySectors.stream().filter(t -> t.getSector() > storm).toList();
+        }
+        List<List<Territory>> returnList = new ArrayList<>();
+        if (!sectorsBeforeStorm.isEmpty()) returnList.add(sectorsBeforeStorm);
+        if (!sectorsAfterStorm.isEmpty()) returnList.add(sectorsAfterStorm);
+        return returnList;
     }
 }
--- src/test/java/model/TerritoriesTest.java
+++ src/test/java/model/TerritoriesTest.java
@@ -4,8 +4,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TerritoriesTest {
     Game game;
@@ -18,7 +19,64 @@ public class TerritoriesTest {
     }
 
     @Test
+    void getTerritory1() {
+        assertEquals(18, territories.getTerritory("Cielago North (West Sector)").getSector());
+    }
+
+    @Test
+    void getTerritory2() {
+        assertThrows(IllegalArgumentException.class, () -> territories.getTerritory("Cielago North"));
+    }
+
+    @Test
     void getDistinctAggregateTerritoryNames() {
         assertEquals(43, territories.getDistinctAggregateTerritoryNames().size());
+    }
+
+    @Test
+    void getTerritorySectors() {
+        List<Territory> territorySectorsList = territories.getTerritorySectors("Cielago North");
+        assertEquals(3, territorySectorsList.size());
+    }
+
+    @Test
+    void getTerritorySectorsForBattleCielagoNoStorm() {
+        List<List<Territory>> territorySectorsList = territories.getTerritorySectorsForBattle("Cielago North", 10);
+        assertEquals(1, territorySectorsList.size());
+        assertEquals(3, territorySectorsList.get(0).size());
+        assertTrue(territorySectorsList.get(0).stream().anyMatch(t -> t.getTerritoryName().equals("Cielago North (West Sector)")));
+        assertTrue(territorySectorsList.get(0).stream().anyMatch(t -> t.getTerritoryName().equals("Cielago North (Center Sector)")));
+        assertTrue(territorySectorsList.get(0).stream().anyMatch(t -> t.getTerritoryName().equals("Cielago North (East Sector)")));
+    }
+
+    @Test
+    void getTerritorySectorsForBattleCielagoStormEast() {
+        List<List<Territory>> territorySectorsList = territories.getTerritorySectorsForBattle("Cielago North", 2);
+        assertEquals(1, territorySectorsList.size());
+        assertEquals(2, territorySectorsList.get(0).size());
+        assertTrue(territorySectorsList.get(0).stream().anyMatch(t -> t.getTerritoryName().equals("Cielago North (West Sector)")));
+        assertTrue(territorySectorsList.get(0).stream().anyMatch(t -> t.getTerritoryName().equals("Cielago North (Center Sector)")));
+    }
+
+    @Test
+    void getTerritorySectorsForBattleCielagoStormCenter() {
+        List<List<Territory>> territorySectorsList = territories.getTerritorySectorsForBattle("Cielago North", 1);
+        assertEquals(2, territorySectorsList.size());
+        assertEquals(1, territorySectorsList.get(0).size());
+        assertEquals(1, territorySectorsList.get(1).size());
+        assertTrue(territorySectorsList.get(0).stream().anyMatch(t -> t.getTerritoryName().equals("Cielago North (West Sector)")));
+        assertTrue(territorySectorsList.get(1).stream().anyMatch(t -> t.getTerritoryName().equals("Cielago North (East Sector)")));
+    }
+
+    @Test
+    void getTerritorySectorsForBattleFWEStormNorth() {
+        List<List<Territory>> territorySectorsList = territories.getTerritorySectorsForBattle("False Wall East", 7);
+        assertEquals(2, territorySectorsList.size());
+        assertEquals(3, territorySectorsList.get(0).size());
+        assertEquals(1, territorySectorsList.get(1).size());
+        assertTrue(territorySectorsList.get(0).stream().anyMatch(t -> t.getTerritoryName().equals("False Wall East (Far South Sector)")));
+        assertTrue(territorySectorsList.get(0).stream().anyMatch(t -> t.getTerritoryName().equals("False Wall East (South Sector)")));
+        assertTrue(territorySectorsList.get(0).stream().anyMatch(t -> t.getTerritoryName().equals("False Wall East (Middle Sector)")));
+        assertTrue(territorySectorsList.get(1).stream().anyMatch(t -> t.getTerritoryName().equals("False Wall East (Far North Sector)")));
     }
 }
