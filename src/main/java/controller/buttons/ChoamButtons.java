d15f3c11fbe94265d3da0f14892676bdab5637ab
TomHayosh <thayosh@gmail.com>
2024-12-29 09:42:21 -0600

    CHOAM buttons stop continued search for button matches

--- src/main/java/controller/buttons/ChoamButtons.java
+++ src/main/java/controller/buttons/ChoamButtons.java
@@ -13,6 +13,9 @@ import java.io.IOException;
 
 public class ChoamButtons implements Pressable {
     public static void press(ButtonInteractionEvent event, Game game, DiscordGame discordGame) throws ChannelNotFoundException, IOException, InvalidGameStateException {
+        // Buttons handled by this class must begin with "choam"
+        // And any button that begins with "choam" must be handled by this class
+
         ChoamFaction choam;
         try {
             choam = (ChoamFaction) game.getFaction("CHOAM");
@@ -20,15 +23,15 @@ public class ChoamButtons implements Pressable {
             return;
         }
 
-        if (event.getComponentId().equals("inflation-double")) {
+        if (event.getComponentId().equals("choam-inflation-double")) {
             choam.setFirstInflation(ChoamInflationType.DOUBLE);
             discordGame.queueMessage("You placed your Inflation token Double side up.");
             discordGame.pushGame();
-        } else if (event.getComponentId().equals("inflation-cancel")) {
+        } else if (event.getComponentId().equals("choam-inflation-cancel")) {
             choam.setFirstInflation(ChoamInflationType.CANCEL);
             discordGame.queueMessage("You placed your Inflation token Cancel side up.");
             discordGame.pushGame();
-        } else if (event.getComponentId().equals("inflation-not-yet")) {
+        } else if (event.getComponentId().equals("choam-inflation-not-yet")) {
             discordGame.queueMessage("You will not place your Inflation token at this time.");
             discordGame.getTurnSummary().queueMessage(Emojis.CHOAM + " does not place their Inflation token at this time.");
         }
