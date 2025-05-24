9862c635cad2bddf3ef795ad849e635cfc472597
TomHayosh <45044702+TomHayosh@users.noreply.github.com>
2025-05-24 08:27:44 -0500

    Refactor Harkonnen Nexus Betrayal and add test. Use in Battles not automated yet. (#362)

--- src/main/java/model/factions/HarkonnenFaction.java
+++ src/main/java/model/factions/HarkonnenFaction.java
@@ -9,11 +9,13 @@ import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.stream.IntStream;
 
 public class HarkonnenFaction extends Faction {
     private Boolean hasTriggeredHT;
     private boolean bonusCardBlocked;
+    protected boolean nexusBetrayalTraitorNeeded;
 
     public HarkonnenFaction(String player, String userName) throws IOException {
         super("Harkonnen", player, userName);
@@ -74,14 +76,29 @@ public class HarkonnenFaction extends Faction {
         game.getTurnSummary().publish(Emojis.HARKONNEN + " has returned " + leader.getName() + " to " + Emojis.getFactionEmoji(leader.getOriginalFactionName()));
     }
 
+    public void nexusCardBetrayal(String traitorName) {
+        LinkedList<TraitorCard> traitorDeck = game.getTraitorDeck();
+        TraitorCard traitorCard = traitorHand.stream()
+                .filter(t -> t.getName().equalsIgnoreCase(traitorName))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Harkonnen does not have " + traitorName + " as a Traitor."));
+        removeTraitorCard(traitorCard);
+        nexusBetrayalTraitorNeeded = true;
+        traitorDeck.add(traitorCard);
+        Collections.shuffle(traitorDeck);
+        ledger.publish(traitorName + " has been shuffled back into the Traitor Deck.");
+        game.getTurnSummary().publish(emoji + " loses " + traitorName + " and will draw a new Traitor in Mentat Pause.");
+    }
+
     @Override
     public void performMentatPauseActions(boolean extortionTokenTriggered) {
         super.performMentatPauseActions(extortionTokenTriggered);
-        if (traitorHand.size() == 3) {
+        if (nexusBetrayalTraitorNeeded) {
             TraitorCard newTraitor = game.getTraitorDeck().pop();
             addTraitorCard(newTraitor);
             ledger.publish(newTraitor.getEmojiNameAndStrengthString() + " is your new Traitor.");
             game.getTurnSummary().publish(Emojis.HARKONNEN + " has drawn a new Traitor.");
+            nexusBetrayalTraitorNeeded = false;
         }
     }
 }
