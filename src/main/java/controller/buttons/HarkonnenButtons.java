0c1798ed48ce00e5d27809c5d5ea2be988d1f1f9
TomHayosh <thayosh@gmail.com>
2026-02-21 11:36:22 -0600

    Fix comments in HarkonnenButtons

--- src/main/java/controller/buttons/HarkonnenButtons.java
+++ src/main/java/controller/buttons/HarkonnenButtons.java
@@ -5,14 +5,13 @@ import controller.commands.SetupCommands;
 import exceptions.ChannelNotFoundException;
 import exceptions.InvalidGameStateException;
 import model.Game;
-import model.factions.HarkonnenFaction;
 import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
 
 import java.io.IOException;
 
 public class HarkonnenButtons implements Pressable {
-    // Buttons handled by this class must begin with "traitor"
-    // And any button that begins with "traitor" must be handled by this class
+    // Buttons handled by this class must begin with "harkonnen"
+    // And any button that begins with "harkonnen" must be handled by this class
     public static void press(ButtonInteractionEvent event, Game game, DiscordGame discordGame) throws ChannelNotFoundException, InvalidGameStateException, IOException {
         if (event.getComponentId().startsWith("harkonnen-mulligan-")) harkonnenMulligan(event, game, discordGame);
     }
@@ -21,8 +20,7 @@ public class HarkonnenButtons implements Pressable {
         String yesOrNo = event.getComponentId().replace("harkonnen-mulligan-", "");
         String response = "You kept your Traitor Hand as is.";
         if (yesOrNo.equals("yes")) {
-            HarkonnenFaction harkonnen = (HarkonnenFaction) ButtonManager.getButtonPresser(event, game);
-            harkonnen.mulliganTraitorHand();
+            game.getHarkonnenFaction().mulliganTraitorHand();
             response = "You mulliganed your Traitor Hand.";
         }
         discordGame.queueDeleteMessage();
